be7496aa293af48edbdc80c90d707eb5bd8b0581
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-12 16:30:50 +0530

    programming exercise - solution

--- 02-programming-constructs/06-assignment.go
+++ 02-programming-constructs/06-assignment.go
@@ -23,3 +23,37 @@ if the user input is not (1-5)
 	print "Invalid choice"
 	IMPORTANT : display the menu again
 */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var n1, n2, userChoice, result int
+	for {
+		fmt.Println("1.Add\n2.Subtract\n3.Multply\n4.Divide\n5.Exit")
+		fmt.Println("Enter your choice :")
+		fmt.Scanln(&userChoice)
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		if userChoice == 5 {
+			break
+		}
+		fmt.Println("Enter the operands :")
+		fmt.Scanln(&n1, &n2)
+		switch userChoice {
+		case 1:
+			result = n1 + n2
+		case 2:
+			result = n1 - n2
+		case 3:
+			result = n1 * n2
+		case 4:
+			result = n1 / n2
+		}
+		fmt.Println("Result :", result)
+	}
+}
