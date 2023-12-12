bec58e45651049d5e3cb56ac8d32dc10779da14c
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-12 15:29:48 +0530

    programming constructs - exercises

---
+++ 02-programming-constructs/04-fmt-scan.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	fmt.Println("Enter your name :")
+	fmt.Scanln(&name)
+	fmt.Printf("Hi %s, Have a nice day!\n", name)
+
+	var x, y int
+	fmt.Scanln(&x, &y)
+	fmt.Println(x, y)
+}
