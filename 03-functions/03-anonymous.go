4a6deffc227c13f62ab6d03a6e832f55cc714f53
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-13 10:09:05 +0530

    more anonymous functions

--- 03-functions/03-anonymous.go
+++ 03-functions/03-anonymous.go
@@ -13,4 +13,19 @@ func main() {
 
 	// convert the fullNameGreet, getFullNameGreet & divide into anonymous functions
 
+	(func(firstName, lastName string) {
+		fmt.Printf("Hi %s %s, Have a nice day!\n", firstName, lastName)
+	})("Magesh", "Kuppan")
+
+	/* with 2 arguments & 1 return result */
+	msg := func(firstName, lastName string) string {
+		return fmt.Sprintf("Hi %s %s, Have a nice day!\n", firstName, lastName)
+	}("Suresh", "Kannan")
+	fmt.Print(msg)
+
+	q, r := func(x, y int) (quotient int, remainder int) {
+		quotient, remainder = x/y, x%y
+		return
+	}(100, 7)
+	fmt.Println(q, r)
 }
