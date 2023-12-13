213624d9119fa9668c8d27f63a5c53981e1be289
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-13 10:37:30 +0530

    functions - assignment - solution

--- 03-functions/04-assignment.go
+++ 03-functions/04-assignment.go
@@ -9,11 +9,9 @@ import "fmt"
 
 func main() {
 
-	var n1, n2, userChoice, result int
+	var userChoice int
 	for {
-		fmt.Println("1.Add\n2.Subtract\n3.Multply\n4.Divide\n5.Exit")
-		fmt.Println("Enter your choice :")
-		fmt.Scanln(&userChoice)
+		userChoice = getUserChoice()
 		if userChoice < 1 || userChoice > 5 {
 			fmt.Println("Invalid choice")
 			continue
@@ -21,18 +19,51 @@ func main() {
 		if userChoice == 5 {
 			break
 		}
-		fmt.Println("Enter the operands :")
-		fmt.Scanln(&n1, &n2)
-		switch userChoice {
-		case 1:
-			result = n1 + n2
-		case 2:
-			result = n1 - n2
-		case 3:
-			result = n1 * n2
-		case 4:
-			result = n1 / n2
-		}
-		fmt.Println("Result :", result)
+		doOperation(userChoice)
 	}
+}
+
+func getUserChoice() (userChoice int) {
+	fmt.Println("1.Add\n2.Subtract\n3.Multply\n4.Divide\n5.Exit")
+	fmt.Println("Enter your choice :")
+	fmt.Scanln(&userChoice)
+	return
+}
+
+func getOperands() (n1, n2 int) {
+	fmt.Println("Enter the operands :")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subntract(x, y int) int {
+	return x - y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
+}
+
+func doOperation(userChoice int) {
+	var result int
+	n1, n2 := getOperands()
+	switch userChoice {
+	case 1:
+		result = add(n1, n2)
+	case 2:
+		result = subntract(n1, n2)
+	case 3:
+		result = multiply(n1, n2)
+	case 4:
+		result = divide(n1, n2)
+	}
+	fmt.Println("Result :", result)
 }
