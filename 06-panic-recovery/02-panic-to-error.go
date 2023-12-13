abeaa9554f616a316e8f32788ef7682f8964465d
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-13 14:13:42 +0530

    panic to error

---
+++ 06-panic-recovery/02-panic-to-error.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrDivideByZero error = errors.New("divide by zero error")
+
+func main() {
+	multiplier := 100
+	divisor := 0
+	q, r, err := divideClient(multiplier, divisor)
+	if err == ErrDivideByZero {
+		fmt.Println("Do not attempt to divide by zero")
+		return
+	}
+	if err != nil {
+		fmt.Println("something went wrong:", err)
+		return
+	}
+	fmt.Printf("Dividing %d by %d, quotient = %d and remainder = %d\n", multiplier, divisor, q, r)
+}
+
+// wrapper to convert the panic into an error
+func divideClient(x, y int) (quotient, remainder int, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = e.(error)
+			return
+		}
+	}()
+	quotient, remainder = divide(x, y)
+	return
+}
+
+// 3rd party library
+func divide(x, y int) (quotient, remainder int) {
+	if y == 0 {
+		panic(ErrDivideByZero)
+	}
+	quotient = x / y
+	remainder = x % y
+	return
+}
