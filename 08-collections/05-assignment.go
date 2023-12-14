2197733a0715a4abf13b61c43a7fdc6de5f1c995
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-14 11:26:08 +0530

    collections - assignment - solution

--- 08-collections/05-assignment.go
+++ 08-collections/05-assignment.go
@@ -1,3 +1,34 @@
-/* Write a function that returns all the prime numbers beween the given start and end
+/*
+	Write a function that returns all the prime numbers beween the given start and end
+
 print the generated prime numbers in the main function one by one
 */
+package main
+
+import "fmt"
+
+func main() {
+	primes := genPrimes(2, 100)
+	for _, primeNo := range primes {
+		fmt.Println(primeNo)
+	}
+}
+
+func genPrimes(start, end int) []int {
+	primes := []int{}
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			primes = append(primes, no)
+		}
+	}
+	return primes
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
--- 08-collections/06-assignment.go
+++ 08-collections/06-assignment.go
@@ -13,6 +13,26 @@ Hint:
 */
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 	str := "Pariatur nisi nostrud id ipsum esse minim velit Lorem eiusmod est reprehenderit sint Esse non proident labore dolore labore nisi dolore dolor proident voluptate minim laborum Ad irure ut Lorem exercitation Ipsum reprehenderit consequat veniam non reprehenderit ut eiusmod magna magna aliquip ut sint Mollit irure fugiat nostrud non excepteur aliquip cillum Labore dolore deserunt irure ea tempor Consequat ea quis ipsum esse minim reprehenderit amet Qui do dolor do anim occaecat culpa commodo sunt mollit excepteur laboris tempor Ullamco labore quis culpa laborum sunt Culpa incididunt ad consectetur ut deserunt tempor proident ut Qui quis aliqua fugiat sunt dolore commodo reprehenderit veniam tempor Voluptate sit laboris sunt et do sint ea irure veniam duis laborum Quis irure id officia inNisi velit proident cupidatat laborum velit enim qui deserunt consectetur Ad ea quis veniam cupidatat tempor sunt elit velit Sit anim irure sunt ut exercitation excepteur elit laboris occaecat dolor Duis ad commodo ut quis nisi pariatur anim Fugiat ad id anim velit labore"
+
+	var maxCount, maxSize int
+	wordsCountBySize := make(map[int]int)
+	words := strings.Split(str, " ")
+	for _, word := range words {
+		key := len(word)
+		wordsCountBySize[key]++
+	}
+
+	for size, count := range wordsCountBySize {
+		if count > maxCount {
+			maxCount, maxSize = count, size
+		}
+	}
+	fmt.Printf("%d letter words occurs the most with %d occurrances\n", maxSize, maxCount)
 }
