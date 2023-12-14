413985c50ca9327418a14a41bfc1b792ea836973
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-14 15:23:22 +0530

    structs & methods - assignment

---
+++ 12-interfaces/01-intro.go
@@ -0,0 +1,14 @@
+package main
+
+type Circle struct {
+	Radius float64
+}
+
+// Define an Area() method for the circle (pi * r * r)
+
+// Define a Rectangle struct (height & width)
+// Defined an Aread() method for the rectangle (height * width)
+
+func main() {
+
+}
