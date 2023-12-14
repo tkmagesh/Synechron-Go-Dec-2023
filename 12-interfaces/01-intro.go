c724d1726fafb73e7d3295340cc796e52451b58f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-14 15:56:04 +0530

    interface composition & using builtin interfaces

--- 12-interfaces/01-intro.go
+++ 12-interfaces/01-intro.go
@@ -1,14 +1,99 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+// v1.0
 type Circle struct {
 	Radius float64
 }
 
-// Define an Area() method for the circle (pi * r * r)
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
 
 // Define a Rectangle struct (height & width)
-// Defined an Aread() method for the rectangle (height * width)
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+// v2.0
+// utility
+
+type AreaFinder interface {
+	Area() float64
+}
+
+func PrintArea(x AreaFinder) {
+	fmt.Println("Area :", x.Area())
+}
+
+// v3.0 (perimeter)
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+type PerimeterFinder interface {
+	Perimeter() float64
+}
+
+func PrintPerimeter(x PerimeterFinder) {
+	fmt.Println("Perimeter :", x.Perimeter())
+}
+
+// v4.0
+type ShapeStatsFinder interface {
+	AreaFinder
+	PerimeterFinder
+}
+
+func PrintShapeStats(x ShapeStatsFinder) {
+	PrintArea(x)
+	PrintPerimeter(x)
+}
+
+// v5.0 (introducing Triangle)
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
+func (t Triangle) Perimeter() float64 {
+	return t.Base + math.Sqrt((t.Base*t.Base)+(4*t.Height*t.Height))
+}
 
 func main() {
+	c := Circle{Radius: 12}
+	// fmt.Println("Area :", c.Area())
+	/*
+		PrintArea(c)
+		PrintPerimeter(c)
+	*/
+	PrintShapeStats(c)
 
+	r := Rectangle{Height: 10, Width: 12}
+	// fmt.Println("Area :", r.Area())
+	/*
+		PrintArea(r)
+		PrintPerimeter(r)
+	*/
+	PrintShapeStats(r)
+
+	t := Triangle{Base: 10, Height: 15}
+	PrintShapeStats(t)
 }
---
+++ 12-interfaces/02-stringer.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Product struct {
+	Id   int
+	Name string
+	Cost float32
+}
+
+/*
+func (p Product) Format() string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %0.2f", p.Id, p.Name, p.Cost)
+}
+*/
+
+// fmt.Stringer interface implementation
+func (p Product) String() string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %0.2f", p.Id, p.Name, p.Cost)
+}
+
+func (p *Product) ApplyDiscount(discountPercentage float32) {
+	p.Cost = p.Cost * ((100 - discountPercentage) / 100)
+}
+
+type PerishableProduct struct {
+	// Dummy
+	Product
+	Expiry string
+}
+
+func NewPerishableProduct(id int, name string, cost float32, expiry string) *PerishableProduct {
+	return &PerishableProduct{
+		Product: Product{
+			Id:   id,
+			Name: name,
+			Cost: cost,
+		},
+		Expiry: expiry,
+	}
+}
+
+// Overriding the "Format()" method of the base struct (Product)
+/*
+func (pp *PerishableProduct) Format() string {
+	return fmt.Sprintf("%s, Expirty = %q", pp.Product.Format(), pp.Expiry)
+}
+*/
+
+func (pp *PerishableProduct) String() string {
+	return fmt.Sprintf("%s, Expirty = %q", pp.Product, pp.Expiry)
+}
+
+func main() {
+	// milk := PerishableProduct{Product{100, "Milk", 50}, "2 Days"} // Not advisable
+	milk := PerishableProduct{
+		Product: Product{
+			Id:   100,
+			Name: "Milk",
+			Cost: 50,
+		},
+		Expiry: "2 Days",
+	}
+
+	/*
+		fmt.Println(milk.Format())
+		milk.ApplyDiscount(10)
+		fmt.Println(milk.Format())
+	*/
+
+	// implicitly using the stringer interface
+	fmt.Println(milk)
+	milk.ApplyDiscount(10)
+	fmt.Println(milk)
+}
